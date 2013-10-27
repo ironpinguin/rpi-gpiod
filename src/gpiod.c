82798b421ec792caf6d63afaf87405597ac70d59
Michele Catalano <michele@catalano.de>
2013-10-27 21:52:04 +0100

    define all commands for lcd.

--- src/gpiod.c
+++ src/gpiod.c
@@ -26,12 +26,21 @@
 #define CLIENT_WRITE   "WRITE"
 #define CLIENT_MODE    "MODE"
 #define CLIENT_LCD     "LCD"
+
+#define LCD_INFO       "INFO"
 #define LCD_CLEAR      "CLEAR"
 #define LCD_SHOW       "SHOW"
 #define LCD_LINE       "LINE"
 #define LCD_RECT       "RECT"
-#define LCD_INIT       "INIT"
 #define LCD_CIRCLE     "CIRCLE"
+#define LCD_ELLIPSE    "ELLIPSE"
+#define LCD_BACKLIGHT  "BACKLIGHT"
+#define LCD_CONTRAST   "CONTRAST"
+#define LCD_DSPNORMAL  "DSPNORMAL"
+#define LCD_INVERT     "INVERT"
+#define LCD_TEXT       "TEXT"
+#define LCD_DOT        "DOT"
+#define LCD_COLOR      "COLOR"
 
 #define SERVER_OK    "OK"
 #define SERVER_ERROR "ERROR"
@@ -39,6 +48,7 @@
 char *socket_filename;
 int flag_verbose     = 0;
 int flag_dont_detach = 0;
+int lcd_is_init      = 0;
 
 void usage() {
   printf("Usage: gpio [ -d ] [ -v ] [ -s socketfile ] [ -h ]\n");
@@ -122,6 +132,15 @@ void write_all_data_to_client(int fd) {
   }
 }
 
+void init_lcd() {
+  if (!lcd_is_init) {
+    // TODO: set di, led and spics per commandline parameter.
+    init(DI, LCD, 0);
+    initFonts();
+    lcd_is_init = 1;
+  }
+}
+
 int is_valid_pin_num(int pin_num) {
   return (pin_num >= 0) && (pin_num < 16);
 }
@@ -140,20 +159,121 @@ void do_lcd_commands(int client_socket_fd, char *buf) {
   int n = sscanf(buf, "%s", command);
   if (n != 1) {
     write_error_msg_to_client(client_socket_fd, "parameter of type string expected");
-  } else if (strcmp(command, LCD_INIT, strlen(LCD_INIT)) == 0) {
-    init(DI, LCD, 0);
-    initFonts();
-  } else if (strcmp(command, LCD_LINE, strlen(LCD_LINE)) == 0) {
+  } else if (strncmp(command, LCD_LINE, strlen(LCD_LINE)) == 0) {
+    init_lcd();
     int n = sscanf(buf, "%s %d %d %d %d", command, &x1, &y1, &x2, &y2);
     if (n != 5) {
       write_error_msg_to_client(client_socket_fd, "unexpected parameters for draw line");
     } else {
       line(x1, y1, x2, y2);
     }
-  } else if (strcmp(command, LCD_SHOW, strlen(LCD_SHOW)) == 0) {
+  } else if (strncmp(command, LCD_SHOW, strlen(LCD_SHOW)) == 0) {
+    init_lcd();
     show();
-  } else if (strcmp(command, LCD_CLEAN, strlen(LCD_CLEAR)) == 0) {
+  } else if (strncmp(command, LCD_CLEAN, strlen(LCD_CLEAR)) == 0) {
+    init_lcd();
     clear();
+  } else if (strncmp(command, LCD_INVERT, strlen(LCD_INVERT)) == 0) {
+    init_lcd();
+    invert();
+  } else if (strncmp(command, LCD_RECT, strlen(LCD_RECT)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d %d %d %d %d", command, &x1, &y1, &x2, &y2, &fill);
+    if (n != 6) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for draw rect");
+    } else {
+      rect(x1, y2, x2, y2, fill);
+    }
+  } else if (strncmp(command, LCD_CIRCLE, strlen(LCD_CIRCLE)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d %d %d %d", command, &x1, &y1, &r1, &fill);
+    if (n != 5) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for draw circle");
+    } else {
+      circle(x1, y1, r1, fill);
+    }
+  } else if (strncmp(command, LCD_ELLIPSE, strlen(LCD_ELLIPSE)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d %d %d %d %d", command, &x1, &y1, &r1, &r2, &fill);
+    if (n != 6) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for draw ellipse");
+    } else {
+      ellipse(x1, y1, r1, r2, fill);
+    }
+  } else if (strncmp(command, LCD_DOT, strlen(LCD_DOT)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d %d", command, &x1, &y1);
+    if (n != 3) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for draw dot");
+    } else {
+      dot(x1, y1);
+    }
+  } else if (strncmp(command, LCD_COLOR, strlen(LCD_COLOR)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d", command, &x1);
+    if (n != 2) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for set pen color");
+    } else if (x1 < 0 || x1 > 1) {
+      write_error_msg_to_client(client_socket_fd, "parameters for set pen color can be only 0 or 1");
+    } else {
+      setPenColor(x1);
+    }
+  } else if (strncmp(command, LCD_BACKLIGHT, strlen(LCD_BACKLIGHT)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d", command, &x1);
+    if (n != 2) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for set backlight");
+    } else if (x1 < 0 || x1 > 1024) {
+      write_error_msg_to_client(client_socket_fd, "parameters for set backlight can be only 0 or 1024");
+    } else {
+      backlight(x1);
+    }
+  } else if (strncmp(command, LCD_CONTRAST, strlen(LCD_CONTRAST)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d", command, &x1);
+    if (n != 2) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for set contrast");
+    } else if (x1 < 5 || x1 > 25) {
+      write_error_msg_to_client(client_socket_fd, "parameters for set contrast can be only 0 or 1024");
+    } else {
+      contrast(x1);
+    }
+  } else if (strncmp(command, LCD_DSPNORMAL, strlen(LCD_DSPNORMAL)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d", command, &x1);
+    if (n != 2) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters for set display normal");
+    } else if (x1 < 0 || x1 > 1) {
+      write_error_msg_to_client(client_socket_fd, "parameters for set display normal can be only 0 or 1");
+    } else {
+      displaynormal(x1);
+    }
+  } else if (strncmp(command, LCD_TEXT, strlen(LCD_TEXT)) == 0) {
+    init_lcd();
+    int n = sscanf(buf, "%s %d %d %d %s", command, &fontId, &x1, &y1, text);
+    if (n != 5) {
+      write_error_msg_to_client(client_socket_fd, "unexpected parameters to write text");
+    } else if (fontId < 0 || fontId > 33) {
+      write_error_msg_to_client(client_socket_fd, "fontId to write Text must be between 0 and 33");
+    } else {
+      selectFont(fontId);
+      writeText(text, x1, y1);
+    }
+  } else if (strncmp(command, LCD_INFO, strlen(LCD_INFO)) == 0) {
+    write_msg_to_client(client_socket_fd, "LCD Commands:");
+    write_msg_to_client(client_socket_fd, "LCD CLEAR => clear screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD SHOW => wirte screen buffer to lcd.");
+    write_msg_to_client(client_socket_fd, "LCD BACKLIGHT value => change backlight between 0 and 1024.");
+    write_msg_to_client(client_socket_fd, "LCD CONTRAST value => change contrast between 5 and 25.");
+    write_msg_to_client(client_socket_fd, "LCD DSPNORMAL value => change display 0 => normal, 1 => reverse.");
+    write_msg_to_client(client_socket_fd, "LCD INVERT => invert display.");
+    write_msg_to_client(client_socket_fd, "LCD DOT x1 y1 => write a dot to the screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD COLOR value => set color of drawing 0 => delete pixel, 1 => write pixel.");
+    write_msg_to_client(client_socket_fd, "LCD LINE x1 y1 x2 y2 => write line to screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD RECT x1 y1 x2 y2 => write rectangle to screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD CIRCLE x1 y1 r1 fill => write circle to screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD ELLIPSE x1 y1 r1 r2 fill => write ellipse to screen buffer.");
+    write_msg_to_client(client_socket_fd, "LCD TEXT fontId x1 y1 \"TEXT\" => write text to screen buffer.");
   } else {
     write_error_msg_to_client(client_socket_fd, "unknown lcd command");
   }
